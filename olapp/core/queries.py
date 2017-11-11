b88b13ddbfaa82e736b50106048103f546846ea3
Jan Bednařík <jan.bednarik@gmail.com>
2017-11-11 23:34:49 +0100

    Extract and use urlsafe Elasticsearch IDs instead of GraphQL Relay IDs.

--- olapp/core/queries.py
+++ olapp/core/queries.py
@@ -1,6 +1,7 @@
 import arrow
 import requests
 import json
+import base64
 
 
 class NotFoundError(Exception):
@@ -15,6 +16,16 @@ def post_query(api_url, query):
     return content['data']
 
 
+def decode_global_id(global_id):
+    type, id = base64.b64decode(global_id).decode('utf-8').split(':')
+    return id
+
+
+def encode_global_id(type, id):
+    global_id = '{}:{}'.format(type, id)
+    return base64.b64encode(global_id.encode('utf-8')).decode('utf-8')
+
+
 def parse_dates(reports):
     for r in reports:
         r['date'] = arrow.get(r['date'])
@@ -26,6 +37,13 @@ def parse_extra(reports):
     for r in reports:
         r['extra'] = json.loads(r['extra'])
         r['author']['extra'] = json.loads(r['author']['extra'])
+    return reports
+
+
+def parse_global_ids(reports):
+    for r in reports:
+        r['id'] = decode_global_id(r['id'])
+        r['author']['id'] = decode_global_id(r['author']['id'])
     return reports
 
 
@@ -51,6 +69,7 @@ def get_all_reports(api_url):
     """
     data = post_query(api_url, query)
     reports = data['reports']
+    reports = parse_global_ids(reports)
     reports = parse_dates(reports)
     reports = parse_extra(reports)
     return reports
@@ -77,12 +96,13 @@ def get_report(api_url, id):
             }}
         }}
     }}
-    """.format(id=id)
+    """.format(id=encode_global_id('Report', id))
     data = post_query(api_url, query)
     report = data['node']
     if report is None:
         raise NotFoundError()
-    reports = parse_dates([report])
+    reports = parse_global_ids([report])
+    reports = parse_dates(reports)
     reports = parse_extra(reports)
     return reports[0]
 
@@ -98,10 +118,11 @@ def get_author(api_url, id):
             }}
         }}
     }}
-    """.format(id=id)
+    """.format(id=encode_global_id('Author', id))
     data = post_query(api_url, query)
     author = data['node']
     if author is None:
         raise NotFoundError()
+    author['id'] = decode_global_id(author['id'])
     author['extra'] = json.loads(author['extra'])
     return author
